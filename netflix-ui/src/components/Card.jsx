a37ca52c2826eb27823cf632d5b7a3e8f9e6731a
AYUSH <89582411+Ayush0031@users.noreply.github.com>
2024-05-13 00:16:04 +0530

    card

--- netflix-ui/src/components/Card.jsx
+++ netflix-ui/src/components/Card.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import styled from 'styled-components'
+import video from "../assets/video.mp4"
+export default function Card({ movieData, isLiked = false }) {
+    const [isHovered, setIsHovered] = useState(false)
+    const navigate=useNavigate();
+    return (
+        <Container
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
 
-export default function Card() {
-  return (
-    <div>
-      
-    </div>
-  )
+            <h4>{movieData.name}</h4>
+            <img src={`https://image.tmdb.org/t/p/w500${movieData.image}`} alt="movie" />
+            {
+                isHovered && (
+                    <div className="hover">
+                        <div className="image-video-container">
+                            <img src={`https://image.tmdb.org/t/p/w500${movieData.image}`} 
+                            alt="movie" 
+                            onClick={()=>navigate('/player')}
+                            />
+                            <video src={video} autoPlay muted loop onClick={()=>navigate('/player')}/>
+                        </div>
+                        <div className="info-container flex column">
+                                <h3 className='name' onClick={()=>navigate('/player')}>
+                                    {movieData.name}
+                                </h3>
+                                <div className="icons flex j-between">
+                                    
+                                </div>
+                                    
+                        </div>
+                    </div>
+                )
+            }
+        </Container>
+    )
 }
+const Container = styled.div``;
--- netflix-ui/src/components/CardSlider.jsx
+++ netflix-ui/src/components/CardSlider.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Card from './Card'
 
-export default function CardSlider() {
+export default function CardSlider({data,title}) {
+
   return (
     <div>
-      
+      {
+        data.map((movie,index)=>{
+            return <Card movieData={movie} index={index} key={movie.id}/>
+        })
+      }
     </div>
   )
 }
