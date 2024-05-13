84b77bb46a0364730bd468d46477704f60231ab2
AYUSH <89582411+Ayush0031@users.noreply.github.com>
2024-05-13 16:19:12 +0530

    card slider

--- netflix-ui/src/components/Slider.jsx
+++ netflix-ui/src/components/Slider.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import CardSlider from './CardSlider'
-
+import styled from 'styled-components'
 export default function Slider({movies}) {
     const getMoviesFromRange=(from,to)=>{
         return movies.slice(from,to)
     }
   return (
-    <div>
+    <Container>
       <CardSlider title="Trending now" data={getMoviesFromRange(0,10)}/>
       <CardSlider title="New Releases" data={getMoviesFromRange(10,20)}/>
       <CardSlider title="Blockbuster Movies" data={getMoviesFromRange(20,30)}/>
       <CardSlider title="Popular On Netflix" data={getMoviesFromRange(30,40)}/>
       <CardSlider title="Action" data={getMoviesFromRange(40,50)}/>
       <CardSlider title="Epics" data={getMoviesFromRange(50,60)}/>
-    </div>
+    </Container>
   )
 }
+const Container=styled.div``;
